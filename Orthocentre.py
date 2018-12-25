ca559375f85f45143e41e276f6d44c588e0cdae5
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2018-12-25 13:13:03 -0500

    Update Orthocentre.py

--- Orthocentre.py
+++ Orthocentre.py
@@ -16,17 +16,17 @@ xSlopeAB = b[0]-a[0] #Run of AB
 ySlopeBC = c[1]-b[1] #Rise of BC
 xSlopeBC = c[0]-b[0] #Run of BC
 
-eqCmidAB = sympy.Eq(xSlopeAB * (x - c[0]) + ySlopeAB * (y - c[1]), 0)
+eqCaltAB = sympy.Eq(xSlopeAB * (x - c[0]) + ySlopeAB * (y - c[1]), 0)
 #Calculates equation of altitude from C to AB
 
-eqAmidBC = sympy.Eq(xSlopeBC * (x - a[0]) + ySlopeBC * (y - a[1]), 0)
+eqAaltBC = sympy.Eq(xSlopeBC * (x - a[0]) + ySlopeBC * (y - a[1]), 0)
 #Calculates equation of altitude from A to BC
 
 print("Point A lies on coordinates: " + str(a))
 print("Point B lies on coordinates: " + str(b))
 print("Point C lies on coordinates: " + str(c))
 
-print("Equation 1: " + str(sympy.simplify(eqCmidAB)))
-print("Equation 2: " + str(sympy.simplify(eqAmidBC)))
+print("Equation 1: " + str(sympy.simplify(eqCaltAB)))
+print("Equation 2: " + str(sympy.simplify(eqAaltBC)))
 
-print("The coordinates of the Triangle's Orthocentre are:\n " + str(sympy.linsolve([eqCmidAB, eqAmidBC], (x, y))))
+print("The coordinates of the Triangle's Orthocentre are:\n " + str(sympy.linsolve([eqCaltAB, eqAaltBC], (x, y))))
